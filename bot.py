e0f477fa22a6feeddee63bdd49100f3dcb58f554
LachlanMalec <50905452+LachlanMalec@users.noreply.github.com>
2021-05-26 18:11:43 +1000

    basic layout of bot

---
+++ bot.py
@@ -0,0 +1,14 @@
+#-------------#
+#setup the bot#
+#-------------#
+
+
+#---------------#
+#create an issue#
+#---------------#
+
+
+#1. read the command
+#2. get the users info
+#3. create the ticket based off that info
+#4. send the ticket to the database
