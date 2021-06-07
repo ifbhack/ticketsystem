0e9bd0c8711083c49d5860fde7b6a8c425d2d2b9
Joshua Turgeon <josh@joshturge.dev>
2021-06-07 23:53:33 +1000

    Fixed syntax errors

--- ticket/schema.sql
+++ ticket/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE [IF NOT EXISTS] [ticket_system].user(
+CREATE TABLE IF NOT EXISTS user(
 	user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	guild_id CHAR(18) NOT NULL,
 	user_name VARCHAR(20) NOT NULL,
 	is_assignable BOOLEAN NOT NULL CHECK(is_assignable IN (0, 1))
 );
 
-CREATE TABLE [IF NOT EXISTS] [ticket_system].ticket(
+CREATE TABLE IF NOT EXISTS ticket(
 	ticket_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	user_id INTEGER NOT NULL,
 	assigned_user_id INTEGER,
@@ -13,16 +13,16 @@ CREATE TABLE [IF NOT EXISTS] [ticket_system].ticket(
 	ticket_description TEXT NOT NULL,
 	ticket_tag VARCHAR(20),
 	ticket_created_on TEXT,
-	FORIEGN KEY(user_id) REFERENCES user(user_id),
-	FORIEGN KEY(assigned_user_id) REFERENCES user(user_id)
+	FOREIGN KEY(user_id) REFERENCES user(user_id),
+	FOREIGN KEY(assigned_user_id) REFERENCES user(user_id)
 );
 
-CREATE TABLE [IF NOT EXISTS] [ticket_system].message(
+CREATE TABLE IF NOT EXISTS message(
 	message_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	ticket_id INTEGER NOT NULL,
 	user_id INTEGER NOT NULL,
 	message_text TEXT NOT NULL,
 	message_sent_on TEXT NOT NULL,
-	FORIEGN KEY(ticket_id) REFERENCES ticket(ticket_id),
-	FORIEGN KEY(user_id) REFERENCES user(user_id)
+	FOREIGN KEY(ticket_id) REFERENCES ticket(ticket_id),
+	FOREIGN KEY(user_id) REFERENCES user(user_id)
 );
