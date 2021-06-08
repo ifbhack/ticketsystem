6d14f75eb4e6ea3adc684986c0df26c5adfed1ed
Joshua Turgeon <josh@joshturge.dev>
2021-06-08 23:24:48 +1000

    Cleaned up types and added get_user method

--- ticket/models/user.py
+++ ticket/models/user.py
@@ -1,27 +1,54 @@
 import sqlite3
 
-class User:
-    user_id = 0
-    guild_id = None
-    username = None
-    is_assignable = False
+class NoUserError(Exception):
+    pass
 
-    def __init__(self, user_id: int, guild_id: str, username: str, is_assignable = False):
+class User:
+    """contains fields related to a user"""
+    user_id: int = 0
+    guild_id: str = ""
+    username: str = ""
+    is_assignable: bool = False
+
+    def __init__(self, user_id: int, guild_id: str, username: str, is_assignable: bool = False):
         self.user_id = user_id
         self.guild_id = guild_id
         self.username = username
         self.is_assignable = is_assignable
 
 class UserModel:
+    """allows a caller to create or retrieve a user"""
+
     _db_conn: sqlite3.Connection
 
     def __init__(self, db_conn: sqlite3.Connection):
         self._db_conn = db_conn
 
-    def create_user(self, guild_id: str, username: str, is_assignable = False):
-        self._db_conn.execute("""
+    def create_user(self, guild_id: str, username: str, is_assignable = False) -> User:
+        """
+        create_user from guild_id and with username. Can also specify if they
+        are assignable to tickets, false by default
+        """
+
+        cursor = self._db_conn.cursor()
+        cursor.execute("""
             INSERT INTO
                 user (guild_id, user_name, is_assignable)
             VALUES(?, ?, ?)
         """, (guild_id, username, is_assignable))
-        self._db_conn.commit()
+
+        return User(cursor.lastrowid, guild_id, username, is_assignable)
+
+    def get_user(self, user_id: int) -> User:
+        """get_user with specified user_id"""
+
+        cursor = self._db_conn.cursor()
+        cursor.execute("""
+            SELECT * FROM user WHERE user_id = ?
+        """, (user_id,))
+
+        row = cursor.fetchone()
+        if row == None:
+            raise NoUserError("user not found")
+
+        return User(int(row[0]), row[1], row[2], bool(row[3]))
