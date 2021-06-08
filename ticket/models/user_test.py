f9d6b86bcab41099f2210f7a34a5a8da4f6754ac
Joshua Turgeon <josh@joshturge.dev>
2021-06-08 23:25:15 +1000

    Added unit tests for the user model

---
+++ ticket/models/user_test.py
@@ -0,0 +1,33 @@
+import unittest
+from ticket import db
+from ticket.models import user
+
+
+# creates an in memory sqlite database for testing methods
+db_filename: str = ":memory:"
+
+class TestMessageModel(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._db_conn = db.create_database(db_filename)
+
+    def setUp(self):
+        self._user_model: user.UserModel = user.UserModel(self._db_conn)
+
+    def __create_test_user(self) -> user.User:
+         return self._user_model.create_user("123456789012345678", "joshturge", True)
+
+    def test_create_user(self):
+        user = self.__create_test_user()
+        queried_user = self._user_model.get_user(user.user_id)
+        self.assertEqual(user.user_id, queried_user.user_id)
+
+    def test_get_user(self):
+        user = self.__create_test_user()
+        queried_user = self._user_model.get_user(user.user_id)
+        self.assertEqual(user.user_id, queried_user.user_id)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._db_conn.close()
