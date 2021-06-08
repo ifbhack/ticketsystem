23d19603a48f8608876711dee766134308e8ea5b
Joshua Turgeon <josh@joshturge.dev>
2021-06-08 10:29:43 +1000

    Implemented user model

--- ticket/user.py
+++ ticket/user.py
@@ -1,23 +1,36 @@
+import sqlite3
+from . import ticket
+
 class User:
     user_id = 0
-    guild_id = 0
+    guild_id = None
     username = None
     is_assignable = False
 
-    def __init__(self, user_id, guild_id, username, is_assignable = False):
+    def __init__(self, user_id: int, guild_id: str, username: str, is_assignable = False):
         self.user_id = user_id
         self.guild_id = guild_id
         self.username = username
         self.is_assignable = is_assignable
 
 class UserModel:
-    _db_conn = None
+    _db_conn: sqlite3.Connection
 
-    def __init__(self, db_conn):
+    def __init__(self, db_conn: sqlite3.Connection):
         self._db_conn = db_conn
 
-    def create_user(self, guild_id, username, is_assignable = False):
-        pass
+    def create_user(self, guild_id: str, username: str, is_assignable = False):
+        self._db_conn.execute("""
+            INSERT INTO
+                user (guild_id, user_name, is_assignable)
+            VALUES(?, ?, ?)
+        """, (guild_id, username, is_assignable))
+        self._db_conn.commit()
 
-    def create_message(self, user, ticket, message_title, message_text):
-        pass
+    def create_message(self, user: User, ticket: ticket.Ticket, message_text: str):
+        self._db_conn.execute("""
+            INSERT INTO
+                message (ticket_id, user_id, message_text)
+            VALUES (?, ?, ?)
+        """, (ticket.ticket_id, user.user_id, message_text))
+        self._db_conn.commit()
