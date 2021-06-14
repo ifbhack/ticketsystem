50be71853ffc58797112fe9f7892549103329d55
Joshua Turgeon <josh@joshturge.dev>
2021-06-14 19:41:49 +1000

    Added base open and view ticket flask functionality

--- ticket/ticket.py
+++ ticket/ticket.py
@@ -1,20 +1,42 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for, flash, g
 
-# EXAMPLE ticket model init:
-# ==========================
-#
-#from ticket.db import get_database
-#from .models.ticket import TicketModel
-#
-#db_conn = get_database()
-#ticket_model = TicketModel(db_conn)
+from ticket.db import get_database
+from ticket.models import TicketModel
 
-bp = Blueprint('ticket', __name__, url_prefix='/ticket')
+bp = Blueprint("ticket", __name__, url_prefix="/ticket")
 
-@bp.route('/open', methods=('GET', 'POST'))
-def open_ticket():
+@bp.before_app_request
+def create_ticket_model():
+    """create_ticket_model and store it in a flask request context """
 
-    # ticket_model can then be used in the routes like:
-    # ticket_model.open_ticket(<params>)
+    db_conn = get_database()
+    g.ticket_model = TicketModel(db_conn)
 
-    return "open ticket"
+@bp.route("/open", methods=("GET", "POST"))
+def open():
+    """ open a ticket and redirect the client to another page """
+
+    if request.method == "POST":
+        user_id = 1
+        title = request.form["title"]
+        description = request.form["description"]
+        tag = request.form["tag"]
+
+        # TODO: check valid user
+
+        ticket = g.ticket_model.open_ticket(user_id, title, description, tag)
+
+        flash(f"Created ticket with ticket id: {ticket.ticket_id}")
+
+        # TODO: change to the index page
+        return redirect(url_for("ticket.view"))
+
+    return render_template("ticket/open.html")
+
+@bp.route("/view", methods=("GET",))
+def view():
+    """ view all of the tickets ever created """
+
+    # TODO: add filter options
+    tickets = g.ticket_model.get_tickets()
+    return render_template("ticket/view.html", tickets=tickets)
